c8c6fcf86d31fb43b90ac01de45d0c2f9e97dfd0
FindHao <find@findhao.net>
2022-02-03 22:44:13 -0500

    change redshow_dummy.cpp to c

--- src/redshow_dummy.c
+++ src/redshow_dummy.c
@@ -5,96 +5,96 @@
 #include <redshow_dummy.h>
 
 
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_output_dir_config_dummy(redshow_analysis_type_t analysis,
                                                                            const char *dir) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_data_type_config_dummy(redshow_data_type_t data_type) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_data_type_get_dummy(redshow_data_type_t *data_type) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_pc_views_get_dummy(uint32_t *views) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_mem_views_get_dummy(uint32_t *views) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_approx_level_config_dummy(redshow_approx_level_t level) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_approx_get_dummy(int *degree_f32, int *degree_f64) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_analysis_enable_dummy(redshow_analysis_type_t analysis_type) {
   return REDSHOW_SUCCESS;
 }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_analysis_enabled_dummy(redshow_analysis_type_t analysis_type) {
   return REDSHOW_SUCCESS;
 }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_analysis_disable_dummy(redshow_analysis_type_t analysis_type) {
   return REDSHOW_SUCCESS;
 }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_analysis_config_dummy(redshow_analysis_type_t analysis_type,
                                                                          redshow_analysis_config_type_t config,
                                                                          bool enable) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_cubin_register_dummy(uint32_t cubin_id, uint32_t mod_id,
                                                                         uint32_t nsymbols, const uint64_t *symbol_pcs,
                                                                         const char *path) {
   return REDSHOW_SUCCESS;
 }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_cubin_cache_register_dummy(uint32_t cubin_id, uint32_t mod_id,
                                                                               uint32_t nsymbols, uint64_t *symbol_pcs,
                                                                               const char *path) {
   return REDSHOW_SUCCESS;
 }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_cubin_unregister_dummy(uint32_t cubin_id, uint32_t mod_id) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_memory_register_dummy(int32_t memory_id, uint64_t host_op_id,
                                                                          uint64_t start, uint64_t end) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_memory_unregister_dummy(uint64_t host_op_id, uint64_t start,
                                                                            uint64_t end) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_memory_query_dummy(uint64_t host_op_id, uint64_t start,
                                                                       int32_t *memory_id, uint64_t *memory_op_id,
                                                                       uint64_t *shadow_start, uint64_t *len) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_memory_ranges_get_dummy(uint64_t host_op_id, uint64_t limit,
                                                                            gpu_patch_analysis_address_t *start_end,
                                                                            uint32_t *len) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_memcpy_register_dummy(int32_t memcpy_id, uint64_t host_op_id,
                                                                          bool src_host, uint64_t src_start, bool dst_host,
                                                                          uint64_t dst_start, uint64_t len) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_memset_register_dummy(int32_t memset_id, uint64_t host_op_id,
                                                                          uint64_t start, uint32_t value, uint64_t len) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_log_data_callback_register_dummy(redshow_log_data_callback_func func) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_record_data_callback_register_dummy(
     redshow_record_data_callback_func func, uint32_t pc_views_limit, uint32_t mem_views_limit) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_analyze_dummy(uint32_t cpu_thread, uint32_t cubin_id, uint32_t mod_id,
                                                                  int32_t kernel_id, uint64_t host_op_id,
                                                                  gpu_patch_buffer_t *trace_data) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_tool_dtoh_register_dummy(redshow_tool_dtoh_func func) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_kernel_begin_dummy(uint32_t cpu_thread, int32_t kernel_id,
                                                                       uint64_t host_op_id) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_kernel_end_dummy(uint32_t cpu_thread, int32_t kernel_id,
                                                                     uint64_t host_op_id) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_analysis_begin_dummy() { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_analysis_end_dummy() { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_flush_thread_dummy(uint32_t cpu_thread) { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_flush_dummy() { return REDSHOW_SUCCESS; }
-[[gnu::noinline]]
+__attribute__((noinline))
 redshow_result_t redshow_flush_now_dummy(uint32_t cpu_thread) { return REDSHOW_SUCCESS; }
